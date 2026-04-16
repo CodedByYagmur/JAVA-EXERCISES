d0e48954f685500b33b8fff93ea12fc2eda43b09
Yağmur <yagmursmseek@gmail.com>
2026-04-16 22:15:29 +0300

    Create MagicalMath.java
    you will see the magical part of math 🪄

---
+++ MagicalMath.java
@@ -0,0 +1,31 @@
+/*This program generates magical math. 
+  Concept of Magical Math is however you change the myNumber 
+  the result never will be different from 3. 
+ */
+public class Magic {
+
+	public static void main(String[] args) {
+		public class Magic {
+			public static void main(String[] args) {
+
+				int myNumber=4532;
+		    //myNumber is our main object.
+
+		    int stepOne= myNumber*myNumber;
+
+		    int stepTwo= stepOne+myNumber;
+
+		    int stepThree= stepTwo/myNumber;
+
+		    int stepFour= stepThree+17;
+
+		    int stepFive=stepFour-myNumber;
+
+		    int stepSix= stepFive/6;
+
+		    System.out.println(stepSix);
+			}
+		}
+	}
+
+}
